fee59d40db4aa7eb8bc3a35cd984270b6e6c6b60
mariefranceperrier <mariefrance.perrier@gmail.com>
2024-04-24 14:11:09 -0400

    implement enumeration candidate

--- filters.rb
+++ filters.rb
@@ -2,15 +2,40 @@
 # This way, we keep these methods separated from other potential parts of the program
 
 def find(id)
-    # Your code Here
-  end
+  @candidates.find { |candidate| candidate[:id] == id }
+end
   
-  def experienced?(candidate)
-    # Your code Here
-  end
+def experienced?(candidate)
+  candidate[:years_of_experience] >= 2
+end
   
-  def qualified_candidates(candidates)
-    # Your code Here
+def ruby_or_python?(candidate)
+  return false if candidate.nil? || candidate[:languages].nil?
+
+  candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python')
+end
+
+def applied_recently?(candidate)
+  candidate[:date_applied] >= 15.days.ago.to_date
+end
+
+def over_17?(candidate)
+  candidate[:age] > 17
+end
+
+def qualified_candidates(candidates)
+  candidates.select do |candidate|
+    experienced?(candidate) &&
+    candidate[:github_points] >= 100 &&
+    ruby_or_python?(candidate) &&
+    applied_recently?(candidate) &&
+    over_17?(candidate)
   end
-  
-  # More methods will go below
+end
+
+def ordered_by_qualifications(candidates)
+ candidates.sort_by do |candidate|
+   [-candidate[:years_of_experience], -candidate[:github_points]]
+ end
+end
+ 
--- main.rb
+++ main.rb
@@ -6,4 +6,27 @@ require './filters'
 
 ## Your test code can go here
 
+# pp find(5)
+candidate = find(5) 
+puts candidate.inspect
+
+# pp experienced?(candidate)
+candidate = @candidates.first  # Get the first candidate from the array
+puts experienced?(candidate)
+
+candidate = @candidates.third  # Get the third candidate from the array
+puts experienced?(candidate)
+
+candidate = @candidates.last  # Get the last candidate from the array
+puts experienced?(candidate)
+
+# pp qualified_candidates(@candidates)
+qualified = qualified_candidates(@candidates)
+puts qualified.inspect
+
+# pp ordered_by_qualifications(@candidates)
+ordered_candidates = ordered_by_qualifications(@candidates)
+puts ordered_candidates.inspect 
+
+
 pp @candidates
